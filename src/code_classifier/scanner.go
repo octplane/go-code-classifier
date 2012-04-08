5e0929827accd7638790c72b8298490e8a5bbf1c
Pierre Baillet <pierre@baillet.name>
2012-04-08 19:56:40 +0200

    More refactor
    Most of the code is now instanciated in Scanner class
    Auto detect type by extension

--- src/code_classifier/scanner.go
+++ src/code_classifier/scanner.go
@@ -13,8 +13,7 @@ import(
 )
 
 const (
-    Ruby bayesian.Class = "ruby"
-    Go bayesian.Class = "go"
+    Invalid bayesian.Class = "invalid"
 )
 
 type Syntax struct {
@@ -26,38 +25,53 @@ type Syntax struct {
 type Scanner struct {
   classifier * bayesian.Classifier
   save_file string
+  name_to_syntax  map[string] *Syntax
+  extensions_to_syntax  map[*regexp.Regexp] *Syntax
+  class_to_syntax map[bayesian.Class] *Syntax
 }
 
-func validLanguages() []  Syntax {
-  s := []Syntax{
-    Syntax{"go",    regexp.MustCompile("go"), bayesian.Class("go")},
-    Syntax{"ruby",  regexp.MustCompile("rb"), bayesian.Class("ruby")}}
+func validLanguages() map[string] *Syntax {
+
+  s := map[string] *Syntax {
+  "go" : &Syntax{"go",    regexp.MustCompile("go"), bayesian.Class("go")},
+  "ruby" : &Syntax{"ruby",  regexp.MustCompile("rb"), bayesian.Class("ruby")}}
 
   return s
 }
 
-
-func InitFromFile(path string, classes ... bayesian.Class ) ( * Scanner) {
+// This is the only constructor you should use
+func InitFromFile(path string) ( * Scanner) {
   log.Printf("Loading %s", path)
+  vl := validLanguages()
+  by_ext := make(map[*regexp.Regexp]*Syntax)
+  by_class := make(map[bayesian.Class]*Syntax)
+
+  for _, syntax := range(vl) {
+    fmt.Printf("Adding %s language.\n", syntax.name)
+    by_ext[syntax.extensions] = syntax
+    by_class[syntax.class] = syntax
+  }
+
   classifier, err := bayesian.NewClassifierFromFile(path)
   if err != nil {
     if os.IsNotExist(err) {
-      classifier = bayesian.NewClassifier(classes ...)
-      if err != nil {
-        log.Fatal(err)
+      classes := make([] bayesian.Class, 0)
+      for re, _ := range by_class {
+        classes = append(classes, re)
       }
+      classifier = bayesian.NewClassifier(classes ...)
     }
-    log.Fatal(err)
   }
-  return &Scanner{classifier, path}
+
+  return &Scanner{classifier, path, vl, by_ext, by_class }
 }
 
 // Scan a file or folder according to a provided Class
-func (scanner * Scanner) Scan(path string, lang bayesian.Class) {
+func (scanner * Scanner) Scan(path string) {
   
   wf := func (path string, info os.FileInfo, err error) error {
     if ! info.IsDir() {
-      scanner.Classify(path, lang)
+      scanner.Classify(path)
     }
     return nil
   }
@@ -72,12 +86,22 @@ func (scanner * Scanner) Snapshot() {
     log.Fatal(err)
   }
 }
-func (scanner * Scanner) Classify(path string, lang bayesian.Class) {
+func (scanner * Scanner) Classify(path string) {
   fmt.Printf("Scanning %s...\n", filepath.Base(path))
-  contents, err := ioutil.ReadFile(path);
-  if err == nil {
-    scanner.classifier.Learn(strings.Split(" ",string(contents)), lang)
-  } else {
-    log.Fatal(err)
+  lang := Invalid
+  for re, syntax:= range(scanner.extensions_to_syntax) {
+    if re.MatchString(filepath.Ext(path)) {
+      lang = scanner.extensions_to_syntax[re].class
+      fmt.Printf("Found a %s file: %s\n", syntax.name, path)
+    }
+  }
+
+  if lang != Invalid {
+    contents, err := ioutil.ReadFile(path);
+    if err == nil {
+      scanner.classifier.Learn(strings.Split(" ",string(contents)), lang)
+    } else {
+      log.Fatal(err)
+    }
   }
 }
--- src/main.go
+++ src/main.go
@@ -4,33 +4,19 @@ import (
   "fmt"
   "os"
   "path/filepath"
-  "github.com/jbrukh/bayesian"
-  "stringslice/betterSlice"
   "code_classifier"
 )
 
-const (
-    Ruby bayesian.Class = "ruby"
-    Go bayesian.Class = "go"
-)
-
 func main() {
-  allowed := betterSlice.StringSlice{"go", "ruby", "python", "perl", "obj-c", "auto"}
 
-  if len(os.Args) == 3 {
-      lang := os.Args[1]
-      path := os.Args[2]
-      fmt.Printf("Will read %s as language: %s.\n", path, lang)
-      if allowed.Pos(lang) > -1 {
-        my_scanner := scanner.InitFromFile("plop.data", Go, Ruby)
-
-        my_scanner.Scan(path, Go)
-        my_scanner.Snapshot()
-      } else {
-        fmt.Printf("%s is not an allowed language among %q.\n", lang, allowed)
-      }
+  if len(os.Args) == 2 {
+      path := os.Args[1]
+      fmt.Printf("Will read %s.\n", path)
+      my_scanner := scanner.InitFromFile("plop.data")
+      my_scanner.Scan(path)
+      my_scanner.Snapshot()
     } else {
-      fmt.Printf("Missing arguments: %s lang path_or_file\n", filepath.Base(os.Args[0]))
+      fmt.Printf("Missing arguments: %s path_or_file\n", filepath.Base(os.Args[0]))
 
     }
 }
