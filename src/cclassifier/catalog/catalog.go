1c35e6c22107f4c69e7a57e0817a93420b23409d
Pierre Baillet <oct@fotonauts.com>
2012-05-16 18:18:00 +0200

    More formatting

--- src/cclassifier/catalog/catalog.go
+++ src/cclassifier/catalog/catalog.go
@@ -1,41 +1,40 @@
 package catalog
 
 import (
-  "encoding/gob"
-  "hash/adler32"
-  "sort"
-  "os"
+	"encoding/gob"
+	"hash/adler32"
+	"os"
+	"sort"
 )
 
 /* a Catalog is just a simple list of uint32 which are in fact
- a Hash32 of the file
+a Hash32 of the file
 */
 
 type Catalog struct {
-  Filename string
-  Files Int32Slice
+	Filename string
+	Files    Int32Slice
 }
 type serializableCatalog struct {
-  files Int32Slice
+	files Int32Slice
 }
 
-type Int32Slice [] uint32
+type Int32Slice []uint32
 
-func (p Int32Slice) Len() int { return len(p) }
-func (p Int32Slice) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p Int32Slice) Len() int           { return len(p) }
+func (p Int32Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p Int32Slice) Less(i, j int) bool { return p[i] < p[j] }
 
-
 func NewCatalogFromFile(path string) (cat *Catalog, err error) {
-  file, err := os.Open(path)
-  if err != nil {
-      return nil, err
-  }
-  dec := gob.NewDecoder(file)
-  w := new(serializableCatalog)
-  err = dec.Decode(w)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	dec := gob.NewDecoder(file)
+	w := new(serializableCatalog)
+	err = dec.Decode(w)
 
-  return &Catalog{path, w.files}, err
+	return &Catalog{path, w.files}, err
 }
 func (c *Catalog) Write() (err error) {
 	file, err := os.OpenFile(c.Filename, os.O_WRONLY|os.O_CREATE, 0644)
@@ -44,19 +43,18 @@ func (c *Catalog) Write() (err error) {
 	}
 	enc := gob.NewEncoder(file)
 	err = enc.Encode(&serializableCatalog{c.Files})
-    return
+	return
 }
 func (c *Catalog) Append(content []byte) {
-  crc := adler32.Checksum([]byte(content))
-  c.Files = append(c.Files, crc)
+	crc := adler32.Checksum([]byte(content))
+	c.Files = append(c.Files, crc)
 }
- func (c *Catalog) Include(content []byte) (ret bool) {
-  crc := adler32.Checksum([]byte(content))
-  sort.Sort(&c.Files)
-  exists := sort.Search(len(c.Files), func(i int) bool { return c.Files[i] >= crc})
+func (c *Catalog) Include(content []byte) (ret bool) {
+	crc := adler32.Checksum([]byte(content))
+	sort.Sort(&c.Files)
+	exists := sort.Search(len(c.Files), func(i int) bool { return c.Files[i] >= crc })
 
-  return exists != len(c.Files)
-  return false
+	return exists != len(c.Files)
+	return false
 
 }
-
--- src/stringslice/betterSlice/pos.go
+++ src/stringslice/betterSlice/pos.go
@@ -3,10 +3,10 @@ package betterSlice
 type StringSlice []string
 
 func (slice StringSlice) Pos(value string) int {
-    for p, v := range slice {
-        if (v == value) {
-            return p
-        }
-    }
-    return -1
+	for p, v := range slice {
+		if v == value {
+			return p
+		}
+	}
+	return -1
 }
