17c76ccb1ce2439e9b4024abaad312a47f7f7c24
Pierre Baillet <pierre@baillet.name>
2012-06-21 00:51:54 +0200

    Improvements
    - CLI parameters support
    - Web server preliminary support
    - Support for more languages

--- src/main.go
+++ src/main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"cclassifier"
+	"flag"
+	"fmt"
+	"html"
 	"log"
-	"os"
-	"path/filepath"
+	"net/http"
 )
 
 func main() {
 
-	if len(os.Args) == 2 {
-		path := os.Args[1]
-		log.Printf("Will read '%s'.\n", path)
-		my_scanner := scanner.InitFromFile(".", "plop")
-		my_scanner.Scan(path)
-		my_scanner.Snapshot()
-	} else {
-		log.Printf("Missing arguments: %s path_or_file\n", filepath.Base(os.Args[0]))
+	var enabledServer = flag.Bool("server", false, "Enable the webserver for request")
+	var serverPort = flag.Int("server-port", 9014, "Port for the webserver")
+	var path = flag.String("path", "", "File or Path")
 
+	flag.Parse()
+
+	if *path != "" {
+		log.Printf("Will read '%s'.\n", *path)
+		my_scanner := scanner.InitFromFile(".", "plop")
+		my_scanner.Scan(*path)
+		my_scanner.Snapshot()
+	} else if *enabledServer {
+		log.Printf("Starting server at port %d\n", *serverPort)
+		http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		})
+
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *serverPort), nil))
+
+	} else {
+		log.Printf("Please provide valid option: path or webserver \n")
+		flag.PrintDefaults()
 	}
 }
